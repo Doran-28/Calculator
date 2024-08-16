9cb13232d92fe4739969ae263b7c9708248f49b4
Doran-28 <116115557+Doran-28@users.noreply.github.com>
2024-08-16 15:00:38 -0400

    Added functionalty for all buttons except for equals

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let varA;
 let varB;
 let operator;
 
+const display = document.querySelector("#display #text");
+let displayText = display.textContent
+
 add = (a, b) => a + b;
 subtract = (a, b) => a - b;
 multiply = (a, b) => a * b;
@@ -20,4 +23,24 @@ function operate(a, b, operator){
     return operations[operator](a, b);
 }
 
-console.log(operate(2, 6, "-"))
+const buttons = Array.from(document.querySelectorAll('#buttons > button')).filter((element) => !element.id);
+
+buttons.forEach(element => {
+    element.addEventListener("click", () => {
+        displayText += element.textContent
+        display.textContent = displayText
+    });
+});
+
+const clearBtn = document.querySelector("#buttons #clear");
+const equalsBtn = document.querySelector("#buttons #equals");
+
+clearBtn.addEventListener("click", () => {
+    displayText = ""
+    display.textContent = displayText
+});
+
+equalsBtn.addEventListener("click", () => {
+    displayText = ""
+    display.textContent = displayText
+});
